55d81a33b41a360147e1d79e774a2b31f931c3d3
Oleandret <oleandret@icloud.com>
2024-11-27 19:37:53 +0100

    Update getWeather.js

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -8,8 +8,22 @@ export async function getWeather(location) {
         const data = await response.json();
 
         if (data.success) {
-            // Returner et ryddig format
-            return `Været i ${data.location}: ${data.condition}, temperatur: ${data.temperature}°${data.unit}. Vind: ${data.details.wind_speed} m/s fra ${data.details.wind_direction}. Fuktighet: ${data.details.humidity}%.`;
+            // Map værtilstander til mer lesbare beskrivelser
+            const conditionMap = {
+                partlycloudy_night: "delvis skyet natt",
+                partlycloudy_day: "delvis skyet dag",
+                clear_day: "klar himmel",
+                clear_night: "klar natt",
+                rainy: "regn",
+                snowy: "snø",
+                cloudy: "overskyet",
+                sunny: "solfylt",
+            };
+
+            const readableCondition = conditionMap[data.condition] || data.condition;
+
+            // Returner et ryddig format med oversatt værtilstand
+            return `Været i ${data.location}: ${readableCondition}, temperatur: ${data.temperature}${data.unit}. Vind: ${data.wind_speed} m/s fra ${data.wind_direction}. Fuktighet: ${data.humidity}%.`;
         } else {
             return `Kunne ikke hente værdata for ${location}.`;
         }
